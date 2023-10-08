63de90e44d7bea5e3502795fb308cf06a3aa11d7
whoami <3652064579@qq.com>
2023-10-08 17:49:57 +0800

    Changes

--- untitled/src/SqlConnect.java
+++ untitled/src/SqlConnect.java
@@ -13,15 +13,17 @@ public class SqlConnect {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");   //加载MYSQL JDBC驱动程序
             //Class.forName("org.gjt.mm.mysql.Driver");
-            System.out.println("成功加载Mysql驱动程序!");
+//            System.out.println("成功加载Mysql驱动程序!");
         } catch (Exception e) {
-            System.out.print("加载Mysql驱动程序时出错!");
+//            System.out.print("加载Mysql驱动程序时出错!");
             e.printStackTrace();
         }
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("成功连接MYSQL服务器！");
             Statement statement = connection.createStatement();
+            statement.execute("insert into stu_info(name,gender,age) values ('小明','男',23)");
+//            statement.execute("delete from stu_info where id=6");
             ResultSet resultSet = statement.executeQuery("select * from stu_info");
             while (resultSet.next()) {
                 System.out.print("name:" + resultSet.getString("name") + "\t");
@@ -31,6 +33,7 @@ public class SqlConnect {
             }
         } catch (Exception e) {
             e.printStackTrace();
+
         }
 
     }
